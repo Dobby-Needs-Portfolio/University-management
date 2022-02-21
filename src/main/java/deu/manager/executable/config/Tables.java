327a8a2798d951c3b235c32989ac61ef41b88047
Mina-1316 <mina1316.canreply@gmail.com>
2022-02-21 19:38:39 +0900

    refactor(config): add the missing table name enum
    table 이름을 저장하는 Enum 클래스 추가

---
+++ src/main/java/deu/manager/executable/config/Tables.java
@@ -0,0 +1,15 @@
+package deu.manager.executable.config;
+
+public enum Tables {
+    AdminStaff("staff_admin"),
+    ClassStaff("staff_class"),
+    Professor("professor"),
+    Student("student"),
+    Major("major"),
+    LectureListener("lecture_listener"),
+    Lecture("lecture");
+
+    private final String value;
+    Tables(String tableName) { this.value = tableName; }
+    public String getValue() { return value; }
+}
