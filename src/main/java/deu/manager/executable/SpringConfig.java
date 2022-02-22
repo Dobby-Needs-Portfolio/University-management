3216ec8a2203a6704a5e745ba038db00c2a2f21d
soochan <tncksdl05@gamil.com>
2022-02-22 22:06:48 +0900

    feat(Major) add MajorRepository Bean in SpringConfig

--- src/main/java/deu/manager/executable/SpringConfig.java
+++ src/main/java/deu/manager/executable/SpringConfig.java
@@ -2,6 +2,8 @@ package deu.manager.executable;
 
 import deu.manager.executable.repository.AdminStaffJdbcRepository;
 import deu.manager.executable.repository.AdminStaffRepository;
+import deu.manager.executable.repository.MajorJdbcRepository;
+import deu.manager.executable.repository.MajorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,4 +25,10 @@ public class SpringConfig {
     public AdminStaffRepository adminStaffRepository(){
         return new AdminStaffJdbcRepository(dataSource);
     }
+
+    @Bean
+    public MajorRepository majorRepository(){
+        return new MajorJdbcRepository(dataSource);
+    }
+
 }
