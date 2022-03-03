4b64ab33a8d8523ae7c8e0960f203ec55a2888ac
Mina-1316 <mina1316.canreply@gmail.com>
2022-03-03 18:16:44 +0900

    test(SpringTest): add logger example test
    log4j2 작동 테스트 코드 추가

--- src/test/java/deu/manager/executable/UniversityAdministrationManagerApplicationTests.java
+++ src/test/java/deu/manager/executable/UniversityAdministrationManagerApplicationTests.java
@@ -1,13 +1,31 @@
 package deu.manager.executable;
 
+import deu.manager.executable.config.Tables;
+import deu.manager.executable.config.exception.DbInsertWrongParamException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UniversityAdministrationManagerApplicationTests {
+	Logger logger = LogManager.getLogger(this.getClass());
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void loggerTest() {
+		logger.info("info");
+		try{
+			throw new DbInsertWrongParamException("error!", Tables.Lecture.getValue());
+		} catch (DbInsertWrongParamException e){
+			logger.warn("exception occurred at: " + e.getTable(), e);
+			logger.error("exception occurred at: " + e.getTable(), e);
+			logger.fatal("exception occurred at: " + e.getTable(), e);
+
+		}
 	}
 
 }
