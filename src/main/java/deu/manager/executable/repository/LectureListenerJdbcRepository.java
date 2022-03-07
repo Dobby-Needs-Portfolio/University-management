4f87fc15ac7e629554896b03a75006314fe59184
Mina-1316 <mina1316.canreply@gmail.com>
2022-03-07 23:57:59 +0900

    docs(LectureListenerRepository): add javadoc
    lecture_listener 메소드 javadoc 추가, 추가할 기능 TODO 추가

--- src/main/java/deu/manager/executable/repository/LectureListenerJdbcRepository.java
+++ src/main/java/deu/manager/executable/repository/LectureListenerJdbcRepository.java
@@ -29,6 +29,12 @@ public class LectureListenerJdbcRepository implements LectureListenerRepository 
         this.jdbc = new JdbcTemplate(dataSource);
     }
 
+    /**
+     * lecture_listener 데이터베이스에 데이터를 저장합니다. 두 parameter 다 null일 수 없습니다.
+     * @param studentId 대상 학생의 id
+     * @param lectureId 대상 강의의 id
+     * @throws DbInsertWrongParamException id가 null로 들어올 시 예외처리
+     */
     //Create
     @Override
     public void save(Long studentId, Long lectureId) throws DbInsertWrongParamException {
@@ -48,6 +54,14 @@ public class LectureListenerJdbcRepository implements LectureListenerRepository 
 
     //Need transaction - 데이터 입력 중 중간에 에러 발생 시 트랜잭션을 통한 rollback 필요
     //나중에 확인할거 - https://stackoverflow.com/questions/39096860/roll-back-a-if-b-goes-wrong-spring-boot-jdbctemplate
+    /**
+     * 일반적인 save 메소드와 같은 기능을 하나, 리스트를 통해 저장 가능합니다. 두 parameter 다 null일 수 없습니다.<br>
+     * 사용에는 반드시 transaction을 병행하여 사용하시기 바랍니다.
+     * @param studentId - 대상 학생의 id
+     * @param lectureId - 대상 강의의 id list
+     * @throws DbInsertWrongParamException id가 null로 들어올 시 예외처리
+     * @see List
+     */
     @Override
     public void save(Long studentId, List<Long> lectureId) throws DbInsertWrongParamException{
         if(studentId == null) {
@@ -76,6 +90,12 @@ public class LectureListenerJdbcRepository implements LectureListenerRepository 
     }
 
     //Read
+
+    /**
+     * 강의의 ID를 통해 강의를 수강하는 학생 리스트를 들고옵니다.
+     * @param lectureId 학생 리스트를 불러오고자 하는 강의의 id
+     * @return 강의를 수강하는 학생 리스트
+     */
     @Override
     public List<Student> searchStudent(Long lectureId) {
         String sql = "SELECT s.id, s.name, s.student_num, s.password, s.resident_num, m.id, m.name " +
@@ -87,6 +107,11 @@ public class LectureListenerJdbcRepository implements LectureListenerRepository 
         return jdbc.query(sql, studentRowMapper(), lectureId);
     }
 
+    /**
+     * 학생의 ID를 통해 학생이 수강하는 강의 리스트를 불러옵니다.
+     * @param studentId 강의 리스트를 불러오고자 하는 학생의 id
+     * @return 학생이 수강하는 강의 리스트
+     */
     @Override
     public List<Lecture> searchLecture(Long studentId) {
         String sql = "SELECT l.id, l.lecture_num, l.name, l.credit_unit, l.max_student, l.min_student, l.is_opened, " +
@@ -100,7 +125,18 @@ public class LectureListenerJdbcRepository implements LectureListenerRepository 
         return jdbc.query(sql, lectureRowMapper(), studentId);
     }
 
+    /* TODO: getBills 메소드 생성/구현.
+    *   학생 ID, 강의 ID를 받거나, 학생 ID를 입력받으면 수강료 리스트 또는 객체를 리턴하는 함수 필요*/
+
     //Update
+    /**
+     * 학생 id, 강의 id로 이루어진 레코드에 수강료 데이터를 추가합니다.<br>
+     * parameter로 입력되는 값들은 null일 수 없습니다.
+     * @param studentId 수정하고자 하는 대상 레코드의 학생 id
+     * @param lectureId 수정하고자 하는 대상 레코드의 강의 id
+     * @param price 추가할 수강료 금액
+     * @throws DbInsertWrongParamException 값이 잘못될 시 예외처리
+     */
     @Override
     public void updatePrice(List<Long> studentId, Long lectureId, Integer price) throws DbInsertWrongParamException{
         if(studentId == null) {
@@ -120,6 +156,12 @@ public class LectureListenerJdbcRepository implements LectureListenerRepository 
         jdbc.update(sql, price, studentId, lectureId);
     }
 
+    /**
+     * 일반적인 lecturelistener와 기능은 동일하나, lectureId에 해당하는 레코드의 값을 <b>전부</b> 수정합니다.
+     * @param lectureId 수정하고자 하는 대상 레코드들의 강의 id
+     * @param price 추가할 수강료 금액
+     * @throws DbInsertWrongParamException - 값이 잘못될 시 예외처리
+     */
     @Override
     public void updatePrice(Long lectureId, Integer price) throws DbInsertWrongParamException{
         if(lectureId == null) {
@@ -136,6 +178,12 @@ public class LectureListenerJdbcRepository implements LectureListenerRepository 
     }
 
     //Delete
+    /**
+     * studentId, lectureID로 이루어진 레코드를 삭제하는 메소드
+     * @param studentId 삭제하고자 하는 대상 레코드의 학생 id
+     * @param lectureId 삭제하고자 하는 대상 레코드의 강의 id
+     * @throws DbInsertWrongParamException 값이 잘못될 시 예외처리
+     */
     @Override
     public void deleteSingle(Long studentId, Long lectureId) throws DbInsertWrongParamException {
         if(studentId == null) {
@@ -150,6 +198,11 @@ public class LectureListenerJdbcRepository implements LectureListenerRepository 
         jdbc.update("DELETE FROM lecture_listener WHERE student_id = ? AND lecture_id = ?", studentId, lectureId);
     }
 
+    /**
+     * 입력된 studentId에 해당되는 모든 레코드를 삭제하는 메소드
+     * @param studentId 삭제하고자 하는 대상 레코드들의 학생 id
+     * @throws DbInsertWrongParamException studentID가 null일 시 예외 발생
+     */
     @Override
     public void deleteStudent(Long studentId) throws DbInsertWrongParamException {
         if(studentId == null) {
@@ -160,6 +213,11 @@ public class LectureListenerJdbcRepository implements LectureListenerRepository 
         jdbc.update("DELETE FROM lecture_listener WHERE student_id = ?", studentId);
     }
 
+    /**
+     * 입력된 studentId 리스트에 해당되는 모든 레코드를 삭제하는 메소드
+     * @param studentId 삭제하고자 하는 대상 레코드들의 학생 id 리스트
+     * @throws DbInsertWrongParamException studentId가 null일 시 발생
+     */
     @Override
     public void deleteStudent(List<Long> studentId) throws DbInsertWrongParamException {
         if(studentId == null) {
@@ -172,6 +230,11 @@ public class LectureListenerJdbcRepository implements LectureListenerRepository 
         namedJdbc.update("DELETE FROM lecture_listener WHERE student_id IN (:student_ids)", param);
     }
 
+    /**
+     * 입력된 lectureId에 해당하는 모든 레코드를 삭제하는 메소드
+     * @param lectureId 삭제하고자 하는 대상 레코드들의 강의 id
+     * @throws DbInsertWrongParamException lectureID가 null일 시 발생
+     */
     @Override
     public void deleteLecture(Long lectureId) throws DbInsertWrongParamException {
         if(lectureId == null) {
@@ -182,6 +245,11 @@ public class LectureListenerJdbcRepository implements LectureListenerRepository 
         jdbc.update("DELETE FROM lecture_listener WHERE student_id = ?", lectureId);
     }
 
+    /**
+     * 입력된 lectureId 리스트에 해당하는 모든 레코드를 삭제하는 메소드
+     * @param lectureId 삭제하고자 하는 대상 레코드들의 강의 id 리스트
+     * @throws DbInsertWrongParamException lectureId가 null일 시 발생
+     */
     @Override
     public void deleteLecture(List<Long> lectureId) throws DbInsertWrongParamException {
         if(lectureId == null) {
