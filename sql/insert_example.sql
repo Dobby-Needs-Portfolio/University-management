5322d440fd27e459f349c4a4277367b5f28820db
Mina-1316 <mina1316.canreply@gmail.com>
2022-03-21 20:22:09 +0900

    refactor(sql): edit example insert to fit regex criteria
    References를 기준으로 설계된 Regexp 조건에 맞게 기존의 예시 비밀번호를 수정함

--- sql/insert_example.sql
+++ sql/insert_example.sql
@@ -1,33 +1,33 @@
 INSERT INTO staff_admin (name, staff_num, password, resident_num)
-VALUE ('어드민', 111, 'admin_password', 1111111111111);
+    VALUE ('어드민', 111, 'adminpw', 1111111111111);
 INSERT INTO staff_class (name, staff_num, password, resident_num)
-VALUE ('직원1', 001, 'staff_password', 1111111111112);
+    VALUE ('직원1', 001, 'staffpw', 1111111111112);
 
 INSERT INTO major (name)
-VALUE ('학과1');
+    VALUE ('학과1');
 INSERT INTO major (name)
-VALUE ('학과2');
+    VALUE ('학과2');
 
 INSERT INTO professor (name, prof_num, password, resident_num, major)
-VALUE ('교수이름1', 001, '123123', '1112221231231', 1);
+    VALUE ('교수이름1', 001, 'profpw1', '1112221231231', 1);
 INSERT INTO professor (name, prof_num, password, resident_num, major)
-VALUE ('교수이름2', 002, '123123', '1112221231231', 2);
+    VALUE ('교수이름2', 002, 'profpw2', '1112221231231', 2);
 
 INSERT INTO student (name, student_num, password, resident_num, major)
-VALUE ('학생이름1', 001, '123123', '1112221231231', 1);
+    VALUE ('학생이름1', 001, 'studpw1', '1112221231231', 1);
 INSERT INTO student (name, student_num, password, resident_num, major)
-VALUE ('학생이름2', 002, '123123', '1112221231232', 2);
+    VALUE ('학생이름2', 002, 'studpw2', '1112221231232', 2);
 
 INSERT INTO lecture (lecture_num, name, credit_unit, professor, max_student, min_student)
-VALUE (1, '강좌1', 3, 1, 30, 15);
+    VALUE (1, '강좌1', 3, 1, 30, 15);
 INSERT INTO lecture (lecture_num, name, credit_unit, professor, max_student, min_student)
-VALUE (2, '강좌2', 2, 2, 30, 15);
+    VALUE (2, '강좌2', 2, 2, 30, 15);
 
 INSERT INTO lecture_listener (lecture_id, student_id)
-VALUE (1, 1);
+    VALUE (1, 1);
 INSERT INTO lecture_listener (lecture_id, student_id)
-VALUE (1, 2);
+    VALUE (1, 2);
 INSERT INTO lecture_listener (lecture_id, student_id)
-VALUE (2, 1);
+    VALUE (2, 1);
 
 
