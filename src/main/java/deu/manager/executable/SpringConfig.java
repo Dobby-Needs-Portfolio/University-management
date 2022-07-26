93efaa060df20a8e6f528ec1d75ed1b48d99a926
soochan <tncksdl05@gmail.com>
2022-07-26 15:29:01 +0900

    refactor : add import package

--- src/main/java/deu/manager/executable/SpringConfig.java
+++ src/main/java/deu/manager/executable/SpringConfig.java
@@ -1,10 +1,9 @@
 package deu.manager.executable;
 
-import deu.manager.executable.repository.AdminStaffJdbcRepository;
+
 import deu.manager.executable.repository.interfaces.AdminStaffRepository;
-import deu.manager.executable.repository.MajorJdbcRepository;
 import deu.manager.executable.repository.interfaces.*;
-import deu.manager.executable.repository.*;
+import deu.manager.executable.repository.jdbc.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
